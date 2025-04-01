56855a3e253e76ea795f481fc92fa92682fb2ab4
lugomez <lugomez@bancolombia.com.co>
2025-04-01 17:16:34 -0500

    refactor saveOrder method to remove unused imports and streamline order processing

--- ms_dapr_processor/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
+++ ms_dapr_processor/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -1,17 +1,13 @@
 package co.com.bancolombia.api;
 
-import co.com.bancolombia.api.dto.OrderDTO;
 import co.com.bancolombia.api.handlers.ValidatorHandler;
 import co.com.bancolombia.api.mapper.OrderDTOMapper;
-import co.com.bancolombia.model.common.exception.GeneralException;
 import co.com.bancolombia.usecase.order.OrderUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-
-import static co.com.bancolombia.model.common.enums.GeneralExceptionMessage.INVALID_BODY_PARAMETER;
 
 @Component
 @RequiredArgsConstructor
@@ -25,12 +21,4 @@ public class Handler {
                 .flatMap(user -> ServerResponse.ok().bodyValue(user));
     }
 
-    public Mono<ServerResponse> saveOrder(ServerRequest serverRequest) {
-        return serverRequest.bodyToMono(OrderDTO.class)
-                .switchIfEmpty(Mono.error(new GeneralException(INVALID_BODY_PARAMETER)))
-                .doOnNext(validatorHandler::validateObject)
-                .map(mapper::toEntity)
-                .flatMap(orderUseCase::saveOrder)
-                .flatMap(order -> ServerResponse.ok().bodyValue(order));
-    }
 }
